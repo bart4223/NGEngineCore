b7cf9fc299d4503d8304e6dafa5e3835625c319c
Nils <nilsgrimmer@gmx.de>
2023-11-02 17:22:17 +0100

    Move

---
+++ src/NGITestableComponent.h
@@ -0,0 +1,19 @@
+//
+//  NGITestableComponent.h
+//  NGEngineCore
+//
+//  Created by Nils Grimmer on 16.01.22.
+//
+
+#ifndef NGITestableComponent_h
+#define NGITestableComponent_h
+
+class NGITestableComponent {
+    
+public:
+    virtual void testSequenceStart();
+    
+    virtual void testSequenceStop();
+};
+
+#endif /* NGITestableComponent_h */
---
+++ src/NGStrings.h
@@ -0,0 +1,21 @@
+//
+//  NGStrings.h
+//  NGEngineCore
+//
+//  Created by Nils Grimmer on 27.12.21.
+//
+
+#ifndef NGStrings_h
+#define NGStrings_h
+
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+char* FloatToChar(float value);
+
+char* LeftPadInteger(int value, int digits);
+
+#endif /* NGStrings_h */
